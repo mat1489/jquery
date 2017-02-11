706ef601e48e400d500b2b42fa6f83713d73c239
Mateusz Fiolek <mat1489@op.pl>
2017-02-11 09:56:01 +0100

    2-selectors

---
+++ 2-selectors/js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+    
+    
+    //pobieranie elementow po znaczniku
+    var paragraphs = $('p');
+    console.log(paragraphs);
+    
+    
+    
+    //pobieranie elementow po id
+    var paragraphOne = $('#paragraf');
+    console.log(paragraphOne);
+    
+    
+    // pobieranie elementow po klasie
+    var paragraphTwo =  $('.paragraf');
+    console.log(paragraphTwo);
+    
+
+
+});
+
