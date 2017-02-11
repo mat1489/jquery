85a3c7d1f7ec5778893e288c7424842be5a2b9af
Mateusz Fiolek <mat1489@op.pl>
2017-02-11 09:25:03 +0100

    1- document ready

---
+++ 1-document ready/js/script.js
@@ -0,0 +1,15 @@
+/*  pelna wersja funkcji document.ready()
+$(document).ready(function(){
+    'use strict';
+   
+    $('#first').css({color: 'red'}).hide(5000).show(3000);
+});
+
+*/
+
+//funkcja document.ready()- wersja skrocona
+$(function(){
+    'use strict';
+   
+    $('#first').css({color: 'red'}).hide(5000).show(3000);
+});
